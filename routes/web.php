4f82c46aa13b3a9767dd2735a22e7f2dfea2e244
Ahmad Sadati <a.sadati336@gmail.com>
2024-01-01 17:19:11 +0330

    adding controllers need some debuging

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\Message; 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
-use App\Models\Message; 
-
+use App\Http\Controllers\BlogController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,34 +27,44 @@ Route::get('/lastshots', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
-Route::post('/send-message', function (Request $request) {
-    DB::table('messages')->insert([
-        'email' => $request->email,
-        'mobile' => $request->mobile,
-        'fullName' => $request->fullName,
-        'message' => $request->message,
-    ]);
-    return
-    '<ul>' .
-    '<li>' . $request->email . '</li>' .
-    '<li>' . $request->mobile . '</li>' .
-    '<li>' . $request->fullName . '</li>' .
-    '<li>' . $request->message . '</li>' .
-        '</ul>';
-        return redirect('/contact');
+// Route::post('/send-message', function (Request $request) {
+//     DB::table('messages')->insert([
+//         'email' => $request->email,
+//         'mobile' => $request->mobile,
+//         'fullName' => $request->fullName,
+//         'message' => $request->message,
+//     ]);
+//     return
+//     '<ul>' .
+//     '<li>' . $request->email . '</li>' .
+//     '<li>' . $request->mobile . '</li>' .
+//     '<li>' . $request->fullName . '</li>' .
+//     '<li>' . $request->message . '</li>' .
+//         '</ul>';
+//         return redirect('/contact');
+// });
+// Route::delete('delete-messages/{id}', function ($id){
+//     $message = Message::find($id);
+//     $message->delete();
+//     return redirect('/contact#messageArea');
+// });
+// Route::get('edit-message/{id}', function ($id) {
+//     $message = Message::find($id);
+//     return view('editMessage', ['message'=>$message]);
+// });
+// Route::put('edit-message/{id}', function (Request $request, $id) {
+//     $message = Message::find($id);
+//     $message->message = $request->message;
+//     $message->save();
+//     return redirect('/contact#messageArea');
+// });
+Route::resource('/messages', MessageController::class);
+Route::resource('/blogs', BlogController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::delete('delete-messages/{id}', function ($id){
-    $message = Message::find($id);
-    $message->delete();
-    return redirect('/contact#messageArea');
-});
-Route::get('edit-message/{id}', function ($id) {
-    $message = Message::find($id);
-    return view('editMessage', ['message'=>$message]);
-});
-Route::put('edit-message/{id}', function (Request $request, $id) {
-    $message = Message::find($id);
-    $message->message = $request->message;
-    $message->save();
-    return redirect('/contact#messageArea');
-});
+
+require __DIR__ . '/auth.php';
